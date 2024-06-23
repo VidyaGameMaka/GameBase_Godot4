007c6ff78441ea791993af5245cf9baa86235a60
VidyaGameMaka <fuzor_silverbolt@hotmail.com>
2024-06-23 15:55:53 -0400

    updates

--- GameBase/GameMaster/Constructors/DiaCstr.cs
+++
@@ -1,8 +0,0 @@
-public  class DiaCstr {
-    public DiaCstr(string text, string voicePath) {
-        this.text = text;
-        this.voicePath = voicePath;
-    }
-    public string text;
-	public string voicePath;
-}
